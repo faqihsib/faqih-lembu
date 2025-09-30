69cefd2a146e7c290b90b74cc8bf8e57b08ef51b
faqih2sib <faqih24si@mahasiswa.pcr.ac.id>
2025-09-30 10:16:00 +0700

    submit form pernyataan

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\MahasiswaController;
@@ -38,3 +39,6 @@ Route::get('/home', [HomeController::class,'index']);
 
 
 Route::get('/pegawai', [PegawaiController::class, 'index']);
+
+Route::post('question/store', [QuestionController::class, 'store'])
+	->name('question.store');
