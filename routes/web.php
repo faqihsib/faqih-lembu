6c2d0d3c19b7d953807f3133d1dd33bacedd3323
faqih2sib <faqih24si@mahasiswa.pcr.ac.id>
2025-10-14 11:28:33 +0700

    crud pt. 6

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatakuliahController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\QuestionController;
+use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -42,3 +45,9 @@ Route::get('/pegawai', [PegawaiController::class, 'index']);
 
 Route::post('question/store', [QuestionController::class, 'store'])
 	->name('question.store');
+
+Route::get('dashboard', [DashboardController::class,'index'])->name('dashboard');
+
+
+//route pelanggan
+Route::resource('pelanggan', PelangganController::class);
